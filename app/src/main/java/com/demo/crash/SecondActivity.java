43024c1585e236bd5906188b6ee57ff50ed024b9
zhangqt <zhangqt@akulaku.com>
2022-03-07 11:13:04 +0800

    androidx

--- app/src/main/java/com/demo/crash/SecondActivity.java
+++ app/src/main/java/com/demo/crash/SecondActivity.java
@@ -3,10 +3,9 @@ package com.demo.crash;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
